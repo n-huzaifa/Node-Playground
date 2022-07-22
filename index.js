f77f1bbe0b138341c309de34e080da49b950d092
Huzaifa <noorhuzaifa11@gmail.com>
2022-07-22 15:46:24 +0500

    event emitter 2.0

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter()
 
-customEmitter.on('emit', () => {
-    console.log('nigga')
+customEmitter.on('emit', (name, id) => {
+    console.log('Hello ' + id + '  ' + name)
 })
 
-customEmitter.emit('emit')
+customEmitter.on('emit', () => {
+    console.log('Hello 2')
+})
 
+
+customEmitter.emit('emit', 'Hello world', 1001)
 
