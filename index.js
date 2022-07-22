fb09b41c206715fb44a60cf4eda689e8c784786f
Huzaifa <noorhuzaifa11@gmail.com>
2022-07-22 12:29:55 +0500

    async await promises

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const { readFile, writeFile } = require('fs').promises;
+
+const start = async () => {
+    try {
+        const first = await readFile('./data.txt', 'utf8');
+        const second = await readFile('./data2.txt', 'utf8');
+        await writeFile('./data3.txt', 'data is cool and pretty good to know about')
+        console.log(first)
+        console.log(second)
+    }
+    catch (error) { console.log(error) }
+}
+
+start()
