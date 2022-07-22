7fa8ffcad6b285e3220599480a4a740a68a7f861
Huzaifa <noorhuzaifa11@gmail.com>
2022-07-22 15:42:33 +0500

    Event Emitters

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const { readFile, writeFile } = require('fs').promises;
+const EventEmitter = require('events')
 
-const start = async () => {
-    try {
-        const first = await readFile('./data.txt', 'utf8');
-        const second = await readFile('./data2.txt', 'utf8');
-        await writeFile('./data3.txt', 'data is cool and pretty good to know about')
-        console.log(first)
-        console.log(second)
-    }
-    catch (error) { console.log(error) }
-}
+const customEmitter = new EventEmitter()
 
-start()
+customEmitter.on('emit', () => {
+    console.log('nigga')
+})
+
+customEmitter.emit('emit')
+
+
