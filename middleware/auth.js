0cd4b71fd8567f90ebf8ed0813caebfe0a867445
n-huzaifa <noorhuzaifa11@gmail.com>
2022-09-26 11:57:43 +0500

    jwt auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = process.env;
+
+function verifyToken(req, res, next) {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+  if (!token) {
+    return res
+      .status(404)
+      .json({ error: "Token is required for authentication" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.AUTH_TOKEN_SECRET);
+    req.user = decoded;
+  } catch (error) {
+    return res.status(401).json({ error: error });
+  }
+  return next();
+}
+
+module.exports = verifyToken;
--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const authRoutes = require("./modules/Auth/Auth.routes");
+const auth = require("./middleware/auth");
 
 router.use("/auth", authRoutes);
+
+router.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
 
 module.exports = router;
